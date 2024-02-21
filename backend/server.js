1f7390364ad1bba8b9e2f9f0d2577bf147a2b0da
Murali Murugan <murali732000@gmail.com>
2024-02-21 17:26:20 +0000

    commit to the server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,26 @@
 const mongoose = require('mongoose');
-const app =require('./index')
-mongoose.connect("mongodb+srv://murali732000:DRNELJ77humGTQIU@cluster0.8pglltf.mongodb.net/Paytm");
+const app = require('./index');
 
+const mongoURI = "mongodb+srv://murali732000:DRNELJ77humGTQIU@cluster0.8pglltf.mongodb.net/Paytm";
 
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Event listeners for Mongoose connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
 
 
 const port = 8080 || 3000;
@@ -12,3 +30,5 @@ const port = 8080 || 3000;
 app.listen(port, () => {
     console.log(`Port is running ${port}`);
 });
+
+
