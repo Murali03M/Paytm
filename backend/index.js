d3242b0c9639dd6a9015c6eed3149a8f4d81c500
Murali Murugan <murali732000@gmail.com>
2024-02-21 18:54:45 +0000

    commit

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,9 @@ const cors = require("cors");
 const userRouter = require('./routes/userRoutes')
 const accountRouter = require('./routes/accountRoutes')
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-
-
-
-
 
 
 app.use('/api/v1/users', userRouter);
@@ -17,7 +13,5 @@ app.use('/api/v1/accounts', accountRouter)
 
 
 module.exports = app;
-
-
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,8 @@
 const mongoose = require('mongoose');
-const app = require('./index');
+const app =require('./index')
+mongoose.connect("mongodb+srv://murali732000:DRNELJ77humGTQIU@cluster0.8pglltf.mongodb.net/Paytm");
 
-const mongoURI = "mongodb+srv://murali732000:DRNELJ77humGTQIU@cluster0.8pglltf.mongodb.net/Paytm";
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: false,
-});
-
-// Event listeners for Mongoose connection
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
-
-mongoose.connection.on('disconnected', () => {
-  console.log('Disconnected from MongoDB');
-});
 
 
 const port = 8080 || 3000;
@@ -30,5 +12,3 @@ const port = 8080 || 3000;
 app.listen(port, () => {
     console.log(`Port is running ${port}`);
 });
-
-
